dc42b481bb7868d8a7558f8a850e7a3b4f2fcf33
Brian <aladebrian@gmail.com>
2025-07-20 14:49:58 -0400

    start on read and list functions

--- main/views.py
+++ main/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, HttpResponse
+from .models import User, Category, Product
 # Create your views here.
+def home(request):
+    return render(request, "base.html")
+
+def list(request):
+    ls = Product.objects.all()
+    return render(request, "list.html", {"products": ls})
